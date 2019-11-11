414200803c71f71dab071a1b4eac2e5a291200b1
Reeadon <lovich8717@hotmail.com>
2019-11-11 14:44:29 +0100

    commentsh

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,7 +13,9 @@ public class Greeter {
   }
 
   /**
-   * This is a method.
+   *
+   * @param someone
+   * @return someone
    */
   final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
