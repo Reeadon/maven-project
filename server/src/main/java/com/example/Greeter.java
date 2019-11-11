9b59e80b0b989e231d6f602a48d96704fc0643b8
Reeadon <lovich8717@hotmail.com>
2019-11-11 14:54:29 +0100

    commentsaji

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,5 +1,12 @@
 /**
- * Jamen den gør det den skal
+ * Domain classes used to produce .....
+ * <p>
+ * These classes contain the ......
+ * </p>
+ *
+ * @since 1.0
+ * @author somebody
+ * @version 1.0
  */
 package com.example;
 
