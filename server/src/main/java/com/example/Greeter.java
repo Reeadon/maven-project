73af3b1ec99f197f97e2c667cdd623b99d29c791
Reeadon <lovich8717@hotmail.com>
2019-11-11 14:48:26 +0100

    commen

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,11 +12,7 @@ public class Greeter {
 
   }
 
-  /**
-   *
-   * @param someone
-   * @return someone
-   */
+  /** {@inheritDoc} */
   final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
